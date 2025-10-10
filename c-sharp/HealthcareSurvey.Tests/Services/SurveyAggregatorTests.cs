d2ee64a9789ca5f8098d227a97497ad993a8b3e0
Jared Wooten <jwooten@nrchealth.com>
2025-10-10 14:03:50 -0400

    r  moved Survey Aggregator Tests into Services folder

--- c-sharp/HealthcareSurvey.Tests/Services/SurveyAggregatorTests.cs
+++ c-sharp/HealthcareSurvey.Tests/Services/SurveyAggregatorTests.cs
@@ -1,7 +1,7 @@
 using HealthcareSurvey.Models;
 using HealthcareSurvey.Services;
 
-namespace HealthcareSurvey.Tests
+namespace HealthcareSurvey.Tests.Services
 {
     public class SurveyAggregatorTests
     {
